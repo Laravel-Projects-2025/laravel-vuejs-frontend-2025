2dfcfd69cfb0ecbfb19704dff3cbcf757063f62e
tony <tonychepa2024@gmail.com>
2025-01-26 16:02:20 +0100

    pagination

--- src/views/posts/PostIndex.vue
+++ src/views/posts/PostIndex.vue
@@ -1,18 +1,34 @@
 <script setup lang="ts">
-
-
 import {onMounted, ref} from "vue";
 import axiosInstance from "@/lib/axios.ts";
-
+import { TailwindPagination } from 'laravel-vue-pagination';
 type Post = {
   id: number;
   title: string;
-  is_published: boolean;
+  slug: string;
+  published: boolean;
+  body: string;
+  createdAt: string;
 }
-const posts = ref<Post[] | null>(null)
+
+type LaravelData = {
+  data: Post[];
+  links: any;
+  meta: any;
+}
+
+const laravelData = ref<LaravelData>({
+  data: [],
+  links: {},
+  meta: {},});
+
+const getResults = async (page = 1) => {
+  const { data} = await axiosInstance.get(`/dashboard/posts?page=${page}`);
+  laravelData.value = await data
+}
+
 onMounted(async  () => {
-  const {data } = await axiosInstance.get("/dashboard/posts");
-  posts.value = data;
+  await getResults();
 })
 </script>
 <template>
@@ -30,13 +46,15 @@ onMounted(async  () => {
           <tr>
             <th scope="col" class="px-6 py-3">ID</th>
             <th scope="col" class="px-6 py-3">Title</th>
+            <th scope="col" class="px-6 py-3">Slug</th>
             <th scope="col" class="px-6 py-3">Published</th>
+            <th scope="col" class="px-6 py-3">Crated</th>
             <th scope="col" class="px-6 py-3">Actions</th>
           </tr>
         </thead>
         <tbody>
-          <template v-if="posts">
-            <tr v-for="post in posts" :key="post.id"
+          <template v-if="laravelData.data?.length > 0">
+            <tr v-for="post in laravelData.data" :key="post.id"
                 class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
             >
               <th
@@ -46,12 +64,18 @@ onMounted(async  () => {
                 {{ post.id}}
               </th>
               <td class="px-6 py-4">{{ post.title}}</td>
-              <td class="px-6 py-4">{{ post.is_published}}</td>
+              <td class="px-6 py-4">{{ post.slug}}</td>
+              <td class="px-6 py-4">{{ post.published}}</td>
+              <td class="px-6 py-4">{{ post.createdAt}}</td>
               <td class="px-6 py-4">Edit/Delete</td>
             </tr>
           </template>
         </tbody>
       </table>
+      <TailwindPagination
+          :data="laravelData"
+          @pagination-change-page="getResults"
+      />
     </div>
   </section>
 </template>
